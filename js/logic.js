111048eb80de064524b38a74812ace3da62bff6c
Emily Adams <tacozcat@Emilys-MacBook-Air.local>
2017-06-03 03:05:08 -0500

    wip

---
+++ js/logic.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+    $(".name").textillate({ in: { effect: 'rollIn' } });
+    $(".skillList").textillate({in: {effect: 'fadeinLeftBig'} });
+});
+// $(document).ready(function() {
+
+//     $('#far-clouds').pan({fps: 30, speed: 0.7, dir: 'left'});
+//     $('#near-clouds').pan({fps: 30, speed: 1, dir: 'left'});
+//     console.log("js working");
+// });
